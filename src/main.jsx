464eca733e9623ea65eb73806f6d5b0c138348e5
The unknvn <mitie931@gmail.com>
2026-08-25 18:02:18 +0200

    feat: add react-query client and wire into app root

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import { NetworkProvider } from "./NetworkContext";
 import { LanguageProvider } from "./LanguageContext";
 import { ThemeProvider } from "./ThemeContext";
+import { queryClient } from "./ui/queryClient";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <NetworkProvider>
-      <LanguageProvider>
-        <ThemeProvider>
-          <App />
-        </ThemeProvider>
-      </LanguageProvider>
-    </NetworkProvider>
+    <QueryClientProvider client={queryClient}>
+      <NetworkProvider>
+        <LanguageProvider>
+          <ThemeProvider>
+            <App />
+          </ThemeProvider>
+        </LanguageProvider>
+      </NetworkProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
